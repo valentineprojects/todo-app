0fa3c5ca49d50b12129ed9ff0749308581fd7b2c
Valentinas <scenass@gmail.com>
2022-02-16 20:03:32 +0200

    commit

--- js/todo.js
+++ js/todo.js
@@ -1,7 +1,17 @@
 
 
-function addNote()
-{
-    var noteValue = document.getElementById("input1").value;
-    return alert(noteValue);
+function addNote(){
+    var li = document.createElement('li');
+    var inputValue = document.getElementById('note-input').value;
+    var textNode = document.createTextNode(inputValue);
+    li.appendChild(textNode);
+    document.getElementById('note-list').appendChild(li);
+
+}
+
+
+function deleteNote(){
+    var list = document.getElementById('add-note');
+    var lastChild = list.lastElementChild();
+    lastChild.remove();
 }
